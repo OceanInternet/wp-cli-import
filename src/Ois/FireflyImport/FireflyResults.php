d910f575727bed68ca033ce65ad0cb1642778309
Andy Weir <andy@uknetweb.com>
2015-02-19 14:05:17 +0000

    debug

--- src/Ois/FireflyImport/FireflyResults.php
+++ src/Ois/FireflyImport/FireflyResults.php
@@ -89,7 +89,7 @@ class FireflyResults extends FireflyImport {
         $result['_wpcf_belongs_sailing-club_id'] = (!empty($this->clubIds["club_{$result['club_id']}"]['ID'])) ? $this->clubIds["club_{$result['club_id']}"]['ID'] : NULL;
         unset($result['club_id']);
 
-        $result['_wpcf_belongs_boat_id'] = (!empty($this->boatIds[$result['sail_no']]['ID'])) ? $this->boatIds[$result['sail_no']]['ID'] : NULL;
+        $result['_wpcf_belongs_boat_id'] = (!empty($this->boatIds[$result['sail_no_']]['ID'])) ? $this->boatIds[$result['sail_no']]['ID'] : NULL;
         unset($result['sail_no']);
 
         $result['_wpcf_belongs_tribe_venue_id'] = (!empty($this->clubIds["club_{$result['venue_id']}"])) ? $this->clubIds["club_{$result['club_id']}"]['_wpcf_belongs_tribe_venue_id'] : NULL;
@@ -156,6 +156,8 @@ class FireflyResults extends FireflyImport {
         }, $clubIds);
 
         $this->clubIds = $this->indexById($clubIds, 'post_name');
+
+        print_r($this->clubIds);
     }
 
     /**
